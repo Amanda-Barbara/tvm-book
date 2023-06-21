db64e03116956ebf38155c948d5c5fa75c4dcd76
xinetzone <xinzone@outlook.com>
2023-06-21 16:19:50 +0800

    添加 TopKAccuracy

--- src/tvm_book/metric/classification.py
+++ src/tvm_book/metric/classification.py
@@ -130,3 +130,55 @@ class Accuracy(EvalMetric):
         num_correct = (pred_labels == labels).sum()
         self.sum_metric += num_correct
         self.num_inst += len(pred_labels)
+
+@dataclass
+class TopKAccuracy(EvalMetric):
+    """计算前 k 个预测的准确性。
+    
+    `TopKAccuracy` 与 `Accuracy` 不同之处在于，只要真实标签在前 K 个预测标签中，就将预测视为“正确”。
+
+    如果 `top_k` = ``1``，那么 `TopKAccuracy` 就与 `Accuracy` 相同。
+
+    Args:
+        top_k: 表示目标是否在前 k 个预测中。
+            
+    Examples:
+        >>> np.random.seed(999)
+        >>> top_k = 3
+        >>> labels = np.array([2, 6, 9, 2, 3, 4, 7, 8, 9, 6])
+        >>> predicts = np.array(np.random.rand(10, 10))
+        >>> acc = TopKAccuracy(top_k=top_k)
+        >>> acc.update(labels, predicts)
+        >>> acc.get()
+        ('top_k_accuracy', 0.3)
+    """
+    top_k: int = 5
+    def __post_init__(self):
+        super().__post_init__()
+        assert(self.top_k > 1), '如果 top_k 为 1，请使用 Accuracy'
+        self.name = f"top_{self.top_k}_accuracy"
+
+    def update(self, labels: np.ndarray, preds: np.ndarray):
+        """更新内部评估结果。
+
+        Args:
+            labels: 数据的标签，以类别索引作为值，每个样本一个标签。
+            preds: 样本的预测值。每个预测值可以是类别索引，也可以是所有类别可能性的向量。
+        """
+        assert(preds.ndim <= 2), '预测结果应该不超过两个维度'
+        # 在这里使用 argpartition 而不是 argsort 是安全的，
+        # 因为我们不关心前 k 个元素的顺序。这样做更快，这很重要，因为由于 Python GIL 的存在，该计算是单线程的。
+
+        preds = np.argpartition(preds.astype('float32'), -self.top_k)
+        labels = labels.astype('int32')
+        num_samples = preds.shape[0]
+        num_dims = len(preds.shape)
+        if num_dims == 1:
+            self.sum_metric += (preds.flat == labels.flat).sum()
+        elif num_dims == 2:
+            num_classes = preds.shape[1]
+            top_k = min(num_classes, self.top_k)
+            for j in range(top_k):
+                num_correct = (preds[:, num_classes - 1 - j].flat == labels.flat).sum()
+                self.sum_metric += num_correct
+        self.num_inst += num_samples
